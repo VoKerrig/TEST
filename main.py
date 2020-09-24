b5edb53486faa70820043f81c5809733e9234f83
VoKerrig <vokerrig@gmail.com>
2020-09-24 19:49:52 +0300

    test2

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ print (str("Vova") + str("krut"))
 x = 3
 y = 4
 print (x + y)
+
+print (str("Vova") + str("krut"))
+x = 3
+y = 4
+print (x * y)
+
+print (str("Vova") + str("krut"))
+x = 3
+y = 4
+print (x / y)
